9afca01512563ebcbe0942c0f3cb199d934fb04b
enda <nope@nope.co.uk>
2019-08-20 21:06:26 +0100

    undid typo fix

--- test-lambda/test.sh
+++ test-lambda/test.sh
@@ -1,1 +1,1 @@
-aws lambda invoke --function-name my_lambda_function  --log-type Tail  --query 'LogResult' --output text --payload '{"pi": "foo", "e":"bar"}' --profile personal | base64 -d
+aws lambda invoke --function-name my_lambda_function out  --log-type Tail  --query 'LogResult' --output text --payload '{"pi": "foo", "e":"bar"}' --profile personal | base64 -d
