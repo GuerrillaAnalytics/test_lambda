7e63190f7c8912d29199d268d0106b9359d2cccc
enda.ridge <nope@nope.co.uk>
2019-08-20 14:57:18 +0100

    code separated into its own function

--- test-lambda1/service.py
+++ test-lambda1/service.py
@@ -1,8 +1,15 @@
 # -*- coding: utf-8 -*-
-
+import os
 
 def handler(event, context):
     # Your code goes here!
-    e = event.get('e')
-    pi = event.get('pi')
-    return e + pi
+    e = event.get("e")
+    pi = event.get("pi")
+    result = my_logic(e, pi)
+    return result
+
+
+def my_logic(first_number, second_number):
+    env_1 = os.environ['env_1']
+    print(env_1)
+    return first_number + second_number
