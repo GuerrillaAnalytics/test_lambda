f329f4faf34007ab3d5e6308311c237339d94943
enda <nope@nope.co.uk>
2019-08-20 21:01:26 +0100

    invoking lambda from aws cli

--- test-lambda1/service.py
+++ test-lambda1/service.py
@@ -1,15 +1,21 @@
 # -*- coding: utf-8 -*-
 import os
+import logging
+logger = logging.getLogger()
+logger.setLevel(logging.INFO)
 
 def handler(event, context):
     # Your code goes here!
+    logger.info(event)
+    
     e = event.get("e")
     pi = event.get("pi")
     result = my_logic(e, pi)
+    print("result= "+ result)
     return result
 
 
 def my_logic(first_number, second_number):
-    env_1 = os.environ['env_1']
-    print(env_1)
+    env_1 = os.environ["env_1"]
+    print("env_1= " + env_1)
     return first_number + second_number
---
+++ test-lambda1/test.sh
@@ -0,0 +1,1 @@
+aws lambda invoke --function-name my_lambda_function out --log-type Tail  --query 'LogResult' --output text --payload '{"pi": "foo", "e":"bar"}' --profile personal | base64 -d
